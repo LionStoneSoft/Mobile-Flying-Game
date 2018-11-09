c9abc0879600ebe84e85242723d2aa4a5113e4f8
Ryan <ryan@ryans-macbook-pro-2.local>
2018-11-09 17:39:53 +0000

    Plane imageView placed with animations

--- Mobile Computing Project/ViewController.swift
+++ Mobile Computing Project/ViewController.swift
@@ -11,16 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var scrollingBackground: UIImageView!
+    @IBOutlet var plane: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundScroll()
+        planeAnimation()
     }
     
     func backgroundScroll() {
         let imageArray = [UIImage(named: "road1.png")!, UIImage(named: "road2.png")!, UIImage(named: "road3.png")!, UIImage(named: "road4.png")!, UIImage(named: "road5.png")!, UIImage(named: "road6.png")!, UIImage(named: "road7.png")!, UIImage(named: "road8.png")!, UIImage(named: "road9.png")!, UIImage(named: "road10.png")!, UIImage(named: "road11.png")!, UIImage(named: "road12.png")!, UIImage(named: "road13.png")!, UIImage(named: "road14.png")!, UIImage(named: "road15.png")!, UIImage(named: "road16.png")!, UIImage(named: "road17.png")!, UIImage(named: "road18.png")!, UIImage(named: "road19.png")!]
         
         scrollingBackground.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
+    }
+    
+    func planeAnimation() {
+        let imageArray = [UIImage(named: "plane1.png")!, UIImage(named: "plane2.png")!, UIImage(named: "plane3.png")!, UIImage(named: "plane4.png")!, UIImage(named: "plane5.png")!, UIImage(named: "plane6.png")!, UIImage(named: "plane7.png")!, UIImage(named: "plane8.png")!, UIImage(named: "plane9.png")!, UIImage(named: "plane10.png")!, UIImage(named: "plane11.png")!, UIImage(named: "plane12.png")!, UIImage(named: "plane13.png")!, UIImage(named: "plane14.png")!, UIImage(named: "plane15.png")!]
+        
+        plane.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
     }
     
 }
