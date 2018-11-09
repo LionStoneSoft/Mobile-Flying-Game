82be8c525eb225c0df7c25cf994ce4465b7bd3a9
Ryan <ryan@ryans-macbook-pro-2.local>
2018-11-09 18:30:02 +0000

    Animated Plane with operating touch class movement

---
+++ Mobile Computing Project/PlaneTouch.swift
@@ -0,0 +1,27 @@
+//
+//  PlaneTouch.swift
+//  Mobile Computing Project
+//
+//  Created by Ryan Soanes on 09/11/2018.
+//  Copyright © 2018 Ryan Soanes. All rights reserved.
+//
+
+import UIKit
+
+class PlaneTouch: UIImageView {
+
+    var startLocation: CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startLocation = touches.first?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
+    }
+
+}
