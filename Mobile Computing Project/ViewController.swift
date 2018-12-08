58dde54d8c88be2425b032c9ea4b9eac903f0a30
Ryan <ryan@ryans-macbook-pro-2.local>
2018-12-08 16:15:56 +0000

    Enemies now fly from right to left

--- Mobile Computing Project/ViewController.swift
+++ Mobile Computing Project/ViewController.swift
@@ -18,7 +18,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         backgroundScroll()
         planeAnimation()
-        crowSpawnTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(enemyAnimation), userInfo: nil, repeats: true)
+        crowSpawnTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(enemyAnimation), userInfo: nil, repeats: true)
+
     }
     
     func backgroundScroll() {
@@ -33,24 +34,29 @@ class ViewController: UIViewController {
         plane.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
     }
     
-    @objc func enemyAnimation() {
-        let number = Int.random(in: -250 ..< 250)
-        
+        @objc func enemyAnimation() {
+        let containerHeight = Int(view.bounds.height)
+        let containerWidth = Int(view.bounds.width)
+        let number = Int.random(in: 0 ..< containerHeight)
+            
+             //667, 375
         let containerView = UIView(
-            frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0)
+            frame: CGRect(x: containerWidth - containerWidth - 75, y: number, width: 0, height: 0)
         )
         
-        let enemy: UIImageView! = UIImageView(frame: CGRect(x: 500, y: number, width: 75, height: 75))
+        let enemy: UIImageView! = UIImageView(frame: CGRect(x: containerWidth, y: number, width: 75, height: 75))
         
         let imageArray = [UIImage(named: "bird1.png")!, UIImage(named: "bird2.png")!, UIImage(named: "bird3.png")!, UIImage(named: "bird4.png")!, UIImage(named: "bird5.png")!, UIImage(named: "bird6.png")!, UIImage(named: "bird7.png")!, UIImage(named: "bird8.png")!, UIImage(named: "bird9.png")!, UIImage(named: "bird10.png")!]
         
         enemy.image = UIImage.animatedImage(with: imageArray, duration: 0.5)
         
-        UIView.animate(withDuration: 2.0, animations: { () -> Void in
+        UIView.animate(withDuration: 5.0, animations: { () -> Void in
             enemy.center = containerView.center
         })
         
         self.view.addSubview(enemy)
+
+        
     }
     
 }
